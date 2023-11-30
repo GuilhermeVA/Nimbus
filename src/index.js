6b2dbc598ee33d182c466e191f81b5edc45e72f9
Guilherme Vallim <guivallimaraujo@gmail.com>
2023-11-30 18:37:23 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -73,13 +73,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Router>
+       <HashRouter>
       <Routes>
         <Route path="/app" element={<App />} />
         <Route path="/" element={<Index />} />
         <Route path="/Historico" element={<Historico />} />
       </Routes>
-    </Router>
+      </HashRouter>
   </React.StrictMode>
 );
 
