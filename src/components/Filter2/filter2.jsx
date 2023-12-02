e74ac6684bc26ad87fbb1d40e310f00b5a51ba9e
Guilherme Vallim <guivallimaraujo@gmail.com>
2023-12-02 18:30:34 -0300

    commit

--- src/components/Filter2/filter2.jsx
+++ src/components/Filter2/filter2.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
-import './filter2.css'; // Certifique-se de ajustar o caminho do arquivo CSS conforme necessário
+import './filter2.css'; 
 import { Link } from 'react-router-dom';
+
+
 
 const FilterForm = () => {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [stationName, setStationName] = useState('');
+  const [operation, setOperation] = useState('');
+  const [frequency, setFrequency] = useState('');
   const [selectedVariables, setSelectedVariables] = useState([]);
 
   const handleStartDateChange = (e) => {
@@ -20,62 +24,83 @@ const FilterForm = () => {
     setStationName(e.target.value);
   };
 
-  const handleVariableClick = (variable) => {
-    // Verifica se a variável já está selecionada
+  const handleOperationChange = (e) => {
+    setOperation(e.target.value);
+  };
+
+  const handleFrequencyChange = (e) => {
+    setFrequency(e.target.value);
+  };
+
+  const handleVariableChange = (variable) => {
     if (selectedVariables.includes(variable)) {
       setSelectedVariables(selectedVariables.filter((v) => v !== variable));
     } else {
-      // Adiciona a variável à lista de variáveis selecionadas
       setSelectedVariables([...selectedVariables, variable]);
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Aqui você pode implementar a lógica de filtragem
-    // Por exemplo, você pode enviar esses valores para um componente pai ou uma função de filtro
-    // Não estou implementando a lógica de filtro neste exemplo, pois você mencionou que não precisa ser funcional
+  
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        Data de Início:
+        Data Inicial:
         <input type="date" value={startDate} onChange={handleStartDateChange} />
       </label>
       <br />
 
       <label>
-        Data de Fim:
+        Data Final:
         <input type="date" value={endDate} onChange={handleEndDateChange} />
       </label>
       <br />
 
       <label>
-        Nome da Estação:
+        Estação:
         <input type="text" value={stationName} onChange={handleStationNameChange} />
       </label>
       <br />
 
       <label>Selecione as Variáveis:</label>
       <br />
+      {['Chuva', 'Umidade', 'Temperatura', 'Velocidade do Vento', 'Visibilidade', 'Temperatura Aparente'].map((variable) => (
+        <span
+          key={variable}
+          className={`variable ${selectedVariables.includes(variable) ? 'selected' : ''}`}
+          onClick={() => handleVariableChange(variable)}
+        >
+          {variable}
+        </span>
+      ))}
+      <br />
 
-      <div className="variable-list">
-        {['Chuva', 'Umidade', 'Temperatura', 'Velocidade do Vento', 'Visibilidade', 'Temperatura Aparente'].map((variable) => (
-          <span
-            key={variable}
-            className={`variable ${selectedVariables.includes(variable) ? 'selected' : ''}`}
-            onClick={() => handleVariableClick(variable)}
-          >
-            {variable}
-          </span>
-        ))}
-      </div>
-      <Link to="/Historico">
-  <button type="submit">Filtrar</button>
+      <label>
+        Operação:
+        <input type="text" value={operation} onChange={handleOperationChange} />
+      </label>
+      <br />
+
+      <label>
+        Frequência:
+        <input type="text" value={frequency} onChange={handleFrequencyChange} />
+      </label>
+      <br />
+
+      
+<Link to="/Historico">
+<button type="submit">Filtrar</button>
 </Link>
+
     </form>
   );
 };
 
 export default FilterForm;
+
+
+
+
